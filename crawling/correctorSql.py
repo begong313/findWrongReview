bee62d65613c836c7ad2d4b2fc587eb2f54975d1
JongBin Noh <95959567+begong313@users.noreply.github.com>
2023-05-01 20:57:32 +0900

    crawling readme 및 주석

--- crawling/correctorSql.py
+++ crawling/correctorSql.py
@@ -1,39 +1,47 @@
 """
 sql에 있는데이터를 가져와 맞춤법 검사 후 다시 반환
 별점과 연계하여 데이터를 추출하기 위해 만들었음.
+hanspell을 설치해도 실행이 잘 안될 경우, GITHUB에서 hanspell을 받아 폴더를 추가해줘야 함.
+
+REVIEW의 INDEX값을 사용하여 데이터들을 가지고 옮으로 INDEX로 나눠 병렬로 실행 가능.
 """
-
-
 import pymysql
 from hanspell import spell_checker
 
-
-dbcon=pymysql.connect(host='localhost', user='root',password='!shwhdqls12',db='yogiyocomment', charset='utf8')
+#db연결 및 데이터 가져오기. 지정한 db명, 테이블이름에 따라 수정
+dbcon=pymysql.connect(host='localhost', user='root',password='password',db='dbname', charset='utf8')
 cur = dbcon.cursor()
 loadingQuery = "select comment from yogiyocomment.reviewdatanew where num=%s"
 updateQuery = "UPDATE yogiyocomment.reviewdatanew set comment = %s where num=%s"
 
+#맞춤법 검사기를 돌릴 수 없는 문장("맞춤법검사 결과가 나오지 않는 문장")시  error파일에 기록 =>수동으로 삭제 또는 수정
 c = open("errlog.txt","a",encoding="utf-8")
 
-i=179588
+#검사를 시작할 INDEX
+i=1
 while i <1000000:
     try:
         cur.execute(loadingQuery,i)
         rs = cur.fetchone()
+
+        #혹시모를 comment가 공백인 review 검사
         if rs==None:
             i+=1
             continue
 
+        #맞춤법 검사후 결과 받아오기
         result = spell_checker.check(rs[0])
         result.as_dict()
 
+        #맞춤법 검사가 완료된 comment 다시 탑제
         cur.execute(updateQuery,(result[2],i))
         dbcon.commit()
 
         print(i)
         i+=1
+
     except:
-        #에러시 그부분 다시실행
+        #에러시 로그에 기록 후 다음 문장 검사 실행
         c.writelines("error : "+ str(i)+"\n")
         i+=1
         continue
--- crawling/main.py
+++ crawling/main.py
@@ -1,21 +1,35 @@
+"""
+요기요 review api url을 활용해 반복문으로 리뷰를 크롤링함.
+요기요 리뷰는 json의 형태로 넘어오며, comment,별점등의 정보 이외에도 많은 정보가 있음.
+현재의 코드는 데이터 중, 각종별점과 comment만 추출함
+
+"""
+
+
 import time
 import requests
 import pymysql
 import re
 
 #db연결
-dbcon=pymysql.connect(host='localhost', user='root',password='!shwhdqls12',db='yogiyocomment', charset='utf8')
+#user,password,db의 값 수정해서 사용바람
+dbcon=pymysql.connect(host='localhost', user='root',password='password',db='dbname', charset='utf8')
 cur = dbcon.cursor()
 
+#api url에 들어가는 식당 고유번호, 고유번호에 해당되는 식당이 없을 수 있음.
 restaurantNum = 1
-#10000까지 돌려보기
 
+#임시로 횟수 제한, 무한반복으로 원하는 만큼 데이터 추출가능.
 while restaurantNum<30500:
     try:
         page = 1
+        #어디까지 진행되었는지 확인하기 위함
         print(restaurantNum)
+
+
         while 1:
             # 요기요 댓글 api url
+            # 리뷰가 한페이지에 10개씩 존재, 리뷰가 더이상 없다면 json객체가 빈 객체로 넘어옴.
             url ="https://www.yogiyo.co.kr/api/v1/reviews/%s/?count=10&only_photo_review=false&page=%s&sort=time"%(restaurantNum,page)
             response = requests.get(url)
             jsonData = response.json()
@@ -24,6 +38,7 @@ while restaurantNum<30500:
             if len(jsonData)==0:
                 break
 
+            #반복문으로 필요한 데이터만 추출.
             for i in range(len(jsonData)):
                 #특수문자 제거
                 comment = re.sub(r"[^\uAC00-\uD7A30-9a-zA-Z\s]", "", str(jsonData[i]['comment']))
@@ -35,15 +50,24 @@ while restaurantNum<30500:
                 delivery = int(jsonData[i]['rating_delivery'])
                 print(comment, totalRating, delivery, taste)
 
+                #EXECUTE 하는 코드
                 sql = "insert into reviewdata (comment, totalRating, taste, quantity, delivery) values (%s, %s,%s, %s, %s)"
                 val = (comment, totalRating,taste, quantity, delivery)
                 cur.execute(sql, val)
+
+            #페이지 변환
             page +=1
+
+            #혹시모를 서버 과부하를 막기위해 넣어 둔 코드.
             time.sleep(0.1)
 
+        #해당 식당의 리뷰가 더이상 존재하지 않을 때 DB에 반영
         dbcon.commit()
+
         #식당고유번호 1 증가
         restaurantNum += 1
+
+        #혹시모를 서버 과부하 방지
         time.sleep(0.1)
     except:
         restaurantNum += 1
